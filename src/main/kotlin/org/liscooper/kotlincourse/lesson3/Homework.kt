7d5405ed423fce8e8b6d119814603df297ee330a
Alisa Kuprava <Lis.cooper606@gmail.com>
2024-09-14 16:38:02 +0200

    commit lesson 3

--- src/main/kotlin/org/liscooper/kotlincourse/lesson3/Homework.kt
+++ src/main/kotlin/org/liscooper/kotlincourse/lesson3/Homework.kt
@@ -3,21 +3,107 @@ package org.liscooper.kotlincourse.lesson3
 class Homework {
     //
 }
-val name: String = "Ironman"
-val date: String = "16/09/2024"
-val venue: String = "Beograd"
-var budget: Int = 1000000000
+val name: String = "Hackathon Survival"
+
+var date: String = "16/09/2024"
+
+val place: String = "Beograd"
+
+private val budget: String = "1000000"
+
+var member: Int = 0
+    get() = field
+    private set(value) {field = value}
+
+var day: Long = 8*60*60
+
+private lateinit var contactinfo: String = "список информации"
+
+var status: String = "не начат"
+
+val sponsors: String = "список спонсоров"
+
+const val BUDGET = 1500000
+
+var internetAccessLevel: Int = 0
+
+private var information: String = "информация о транспартировке"
+
+var numberOfCommands: Int = 0
+
+lateinit var tasks: String = "задачи"
+
+val evacuationPlan: String = "план эвакуации"
+
+var listOfAvailableEquiment: String = "50"
+
+var listOfFreeEquipment: String = "50"
+
+lateinit var mealShedule: String = "будет известно после назначения поставщика"
+
+val planInCaseOfFailures: String = "план мероприятия на случай сбоев"
+
+private val listOfExpertsAndJury: String = "список экспертов и жюри"
+
+var methodsForCollectingFeedback: String = "онлайн опросы и интервью"
+
+val privacyPolicy: String = "политика конфиденциальности"
+
+lateinit var privateReviews: String = "приватные отзывы"
+
+lateinit var temperatureIndoor: Int = 25
+
+var networkEquipmentPerformance: String = "мониторинг и анализ данных"
+
+var lumensLevl: Int = 1100
+
+private var log: String = ""
+
+const val ambulance: String = "доступно"
+
+private val emergencyProtocols: String = "протоколы"
+
+val namber: Int = 598
+
+val dicebelMax: Int = 55
+
+
+
+var eventFormat: String = "открытый"
+
+var numberOfAvailableSeats: Int = 50
     get () = field
     set (value) {
-        if (value >= 1500000000) field = value
+        if (value >= 0) field = value
     }
-const val PARTICIPANTS: Int = 250
-var day: Int =  2
-lateinit var userinfo: String
-val lazyValue: String by lazy {"Status at the beginning of the event"}
-lateinit var sponsors: String
-const val BUDGET = 1500000000
-var internet: Double = 90.4
-lateinit var info: String
-const val TEAMS: Int = 8
-lateinit var tasks: String
+
+val pressRelationsPlan: String = "подробный план"
+
+lateinit var detailedInformationAboutProjects: String = "сбор данных"
+
+lateinit var permitStatus: String = "не получены"
+
+private var exclusiveResources: String = "не получены"
+
+val listOfEventPartners: String = "список партнеров"
+
+val eventReport: String by lazy {"генерируется после завершения мероприятия"}
+
+val prismDistributionPlan: String = "план"
+
+private val emergencyContactInformation: String = "список контактов"
+
+var specialConditionsForParticipantsWithDisabilities: String = "список условий"
+
+val mood: String by lazy {""}
+
+lateinit var plan: String
+
+lateinit var specialGuest: String
+
+var maximumNumberOfGuests: Int = 150
+    get () = field
+    set (value) {
+        if (value >= 0) field = value
+
+var numberOfHoursForEachTeam: Int = 2
